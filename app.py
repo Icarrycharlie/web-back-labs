424174fe5c562eaaffea643be39925757466b881
Максим Гришти <maxgrexti@yandex.ru>
2025-11-21 01:42:57 +0700

    Пятая лабораторная

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, url_for, request, redirect, abort, render_template
+from flask import Flask, url_for, request, redirect, abort, render_template, session
+import os
 from datetime import datetime
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 app = Flask(__name__)
-app.secret_key =  'секретно-секретный ключ'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route("/")
 @app.route("/index")
@@ -29,6 +33,7 @@ def index():
                 <li><a href="''' + url_for('lab2.lab') + '''">Вторая лабораторная</a></li>
                 <li><a href="''' + url_for('lab3.lab') + '''">Третья лабораторная</a></li>
                 <li><a href="''' + url_for('lab4.lab') + '''">Четвертая лабораторная</a></li>
+                <li><a href="''' + url_for('lab5.lab') + '''">Пятая лабораторная</a></li>
             </menu>
         </main>
         <footer>
